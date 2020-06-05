a8e2cd81bda06b66617a83aeb71b6c83cb69522f
dev-promtheus98 <ozh98@protonmail.com>
2020-06-05 01:25:32 +0000

    Adding administration panel for user

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,9 +8,30 @@
                 <div class="card-header">Liste des utilisateurs</div>
 
                 <div class="card-body">
-                    @foreach ($users as $user)
-                        {{ $user->name }} - {{ $user->email }}
-                    @endforeach
+                    <table class="table w-100">
+                        <thead class="thead-light">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Nom</th>
+                                <th scope="col">Email</th>
+                                <th scope="col">Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <th scope="row">{{ $user->id }}</th>
+                                    <td>{{ $user->name }}</td>
+                                    <td>{{ $user->email }}</td>
+                                    <td>
+                                        <a href="{{ route('admin.users.edit', $user->id) }}"><button class="btn btn-primary">Editer</button></a>
+                                        <a href="{{ route('admin.users.destroy', $user->id) }}"><button class="btn btn-warning">Supprimer</button></a>
+                                    </td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                      </table>
+
                 </div>
             </div>
         </div>
